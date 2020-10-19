dfd593f8f66e71391af471f16c01ea6811784572
behulum <behulummd2018@gmail.com>
2020-10-18 21:12:39 -0400

    CD offering change

--- src/main/java/com/meritamerica/assignment3/CDOffering.java
+++ src/main/java/com/meritamerica/assignment3/CDOffering.java
@@ -1,53 +1,32 @@
 package com.meritamerica.assignment3;
-
-/* Week 4 Partner Pair Assignment #3
- * Bank Account Part 3  
- * @date October 17, 2020
- * PART 3
- */
-
-
 public class CDOffering {
-
-	int term;
-	double interestRate;
-	
-
-// CONSTRUCTOR
-
-	public CDOffering (int term, double interestRate) {
+	private int term;
+	private double interestRate;
+	//1. CDOferring (int term, double interestRate)
+	// behulum and allana
+	public CDOffering(int term, double interestRate) {
 		this.term = term;
 		this.interestRate = interestRate;
 	}
-		
-	
+	//2. int getTerm ()
 	public int getTerm() {
-		return term;
+		return this.term;
 	}
-	
+	//3. double getInterestRate ()
 	public double getInterestRate() {
-		return interestRate;
+		return this.interestRate;
 	}
-	
-	public String toString() {
-		return "Your CD Offering Terms: " + this.getTerm() + " Your CD Offering Interest Rate: " + this.getInterestRate();
+	public String writeToString(){
+		return term+","+interestRate;
 	}
-		
-//------------------------------- ASSIGNMENT 3 AMENDMENTS ------------------------------
-	
-	static CDOffering readFromString(String cdOfferingDataString) {
-		
-		String storage1[] = cdOfferingDataString.split(",");
-		int testTerm = Integer.parseInt(storage1[0]);
-		double testInterest = Double.parseDouble(storage1[1]);
-		
-		CDOffering offeringA = new CDOffering(testTerm, testInterest);
-		return offeringA;
+	public static CDOffering readFromString(String cdOfferingInfo) {
+		String[] cdInfo =  cdOfferingInfo.split(",");
+		CDOffering cDOffering = new CDOffering(Integer.getInteger(cdInfo[0]), Double.valueOf(cdInfo[1]));
+		return cDOffering;
 	}
-	
-	
-	public String writeToString() {
-		return this.term + "," + this.interestRate;
-	}
-	
 }
+
+
+
+
+
